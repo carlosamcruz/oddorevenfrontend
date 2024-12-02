d6686c81338e51c27197820e9191ce53d69a459e
carlosamcruz <carlosamcruz@ufam.edu.br>
2024-12-02 17:11:44 -0300

    Game Page and Functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,239 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import Header from './Header';
+import { gameState, initGame, insertGameKey, insertPlayerOption, Options, gameData, cancel, acceptGame, resultGame} from './Web3Service';
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [gameKey, setGameKey] = useState("0000000000000000000000000000000000000000000000000000000000000000");
+
+  const [playerOption, setPlayerOption] = useState(Number);
+  const [gameDataThis, setGameDataThis] = useState(gameData);
+
+  useEffect(()=>{  
+    gameState()
+      .then(state => setGameDataThis(state))
+      .catch(err => setMessage(err.message));
+
+  }, []);
+  
+  function onInputGameKey(event: React.ChangeEvent<HTMLInputElement>){
+    setGameKey(event.target.value);
+  }
+
+  function onInputPlayerOption(event: React.ChangeEvent<HTMLInputElement>){
+    setPlayerOption(Number(event.target.value));
+  }
+  
+
+  function onChangeGameKey(){
+
+    console.log('GameData: ', gameDataThis);
+
+    insertGameKey(gameKey)
+      .then(tx => setMessage(tx))
+      .catch(err => setMessage(err.message)); 
+  }
+
+  function onChangeP1Option(){
+
+    insertPlayerOption(playerOption, true)
+      .then(option => setMessage(String(option)))
+      .catch(err => setMessage(err.message)); 
+
+  }
+
+  function onChangeP2Option(){
+
+    insertPlayerOption(playerOption, false)
+      .then(option => setMessage(String(option)))
+      .catch(err => setMessage(err.message)); 
+
+  }
+
+
+  function onPlay(option: Options){
+    setMessage("");
+    initGame(option == 1 ? true: false)
+      .then(state => setGameDataThis(state))
+      //.then(result => setLeaderboard({...leaderboard, result}))
+      .catch(err => setMessage(err.message));
+  }
+
+  function onCancel(){
+    setMessage("");
+    cancel()
+      .then(state => setGameDataThis(state))
+      //.then(result => setLeaderboard({...leaderboard, result}))
+      .catch(err => setMessage(err.message));
+  }
+
+  function onAccept(){
+    setMessage("");
+    acceptGame()
+      .then(state => setGameDataThis(state))
+      //.then(result => setLeaderboard({...leaderboard, result}))
+      .catch(err => setMessage(err.message));
+  }
+
+  function onResult(){
+    setMessage("");
+    resultGame()
+      .then(state => setGameDataThis(state))
+      //.then(result => setLeaderboard({...leaderboard, result}))
+      .catch(err => setMessage(err.message));
+  }
+
+  function onClaim(){
+    setMessage("");
+    resultGame()
+      .then(state => setGameDataThis(state))
+      //.then(result => setLeaderboard({...leaderboard, result}))
+      .catch(err => setMessage(err.message));
+  }
+
+  return (
+    <div className='container'>
+        <Header />
+        <main>
+          <div className='py-0 text-center'>
+
+            <img className='d-block mx-auto mb-4' src='./logo512.png' alt='OddOrEven' width={72}/>
+            <h2> Odd or Even </h2>
+            <p className='lead'> Play the game. </p>
+            <p className='lead text-danger'> {message} </p>
+          </div>
+          <div className='col-md-8 col-lg-12'>
+            <div className='row'>
+
+              <div className='py-5 col-sm-6'>
+                <div className='col-sm-12'> 
+                  <label htmlFor='gamekey' className='form-label'> Player 1 Game Key (hex):</label>
+                  <div className='input-group'>
+                    <input type='hex' className='form-control' id='gamekey' value={gameKey || ""} onChange={onInputGameKey}/>
+                    <span className='input-group-text bg-secondary'>hex</span>
+                    <button type='button' className='btn btn-primary d-inline-flex align-items-center' onClick={onChangeGameKey}>Insert</button>
+
+                  </div>
+                </div>
+                <div className='py-5 col-sm-12'> 
+                  <label htmlFor='playerOption' className='form-label'> Player Option (0 to 10):</label>
+                  <div className='input-group'>
+                    <input type='number' className='form-control' id='gamekey' value={playerOption || 0} onChange={onInputPlayerOption}/>
+                    <span className='input-group-text bg-secondary'>num</span>
+                    {
+                      gameDataThis.hashOptionP1 == "0x0000000000000000000000000000000000000000000000000000000000000000"  ?
+                      <button type='button' className='btn btn-primary d-inline-flex align-items-center' onClick={onChangeP1Option}>Insert</button>
+                      :
+                      gameDataThis.optionP2 == -1?
+                      <button type='button' className='btn btn-primary d-inline-flex align-items-center' onClick={onChangeP2Option}>Insert</button>
+                      :
+                      <button type='button' className='btn btn-primary d-inline-flex align-items-center' onClick={onChangeP1Option}>Insert</button>
+
+                    }
+
+                  </div>
+                </div>
+              </div>
+              <div className='col-sm-6'>
+                <h4 className='mb-0'>Games</h4>
+                <div className='card card-body border-0 shadow'>
+                  <h5 className='mb-3 text-primary'>
+                    {  gameDataThis.hashOptionP1 == "0x0000000000000000000000000000000000000000000000000000000000000000"  ? 
+                      "Initialize Game:"
+                      :
+                      gameDataThis.optionP2 == -1?
+                      "Hash Player 1:"
+                      :
+                      "Player 2 Option"
+                    }
+                    </h5>
+                  <div className='alert alert-success'>
+                    {
+                      //"Loading ..."
+                      gameDataThis.hashOptionP1 == "0x0000000000000000000000000000000000000000000000000000000000000000"  ? 
+                      "Insert Game Key and Number Choice:"
+                      :
+                      gameDataThis.optionP2 == -1?
+                      gameDataThis.hashOptionP1
+                      :
+                      gameDataThis.optionP2
+                    }
+                  </div>
+                  <h5 className='mb-3 text-primary'>
+                    {
+                      //leaderboard && leaderboard.result?.indexOf("won") !== -1 || !leaderboard?.result
+                      //? "Start a new game:"
+                      //:
+                      gameDataThis.hashOptionP1 == "0x0000000000000000000000000000000000000000000000000000000000000000"  ? 
+                      "Choose Odd or Even:"
+                      :
+                      gameDataThis.optionP2 == -1?
+                      "Accept or Quit Game"
+                      :
+                      "Result or Claim Game"
+                    }
+                  </h5>
+                    {
+                      gameDataThis.hashOptionP1 == "0x0000000000000000000000000000000000000000000000000000000000000000"  ?
+                      <div className='d-flex'>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info me-3 play-button' onClick={() => onPlay(Options.ODD)}>
+                            <img src='/assets/odd.png' width={100} alt='Odd'/>
+                          </div>
+                        </div>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info ms-3 play-button' onClick={() => onPlay(Options.EVEN)}>
+                            <img src='/assets/even.png' width={100} alt='Even'/>
+                          </div>
+                        </div>
+                      </div>
+                      :
+                      gameDataThis.optionP2 == -1?
+                      <div className='d-flex'>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info me-3 play-button' onClick={() => onAccept()}>
+                            <img src='/assets/play.png' width={100} alt='Play'/>
+                          </div>
+                        </div>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info ms-3 play-button' onClick={() => onCancel()}>
+                            <img src='/assets/cancel.png' width={100} alt='Cancel'/>
+                          </div>
+                        </div>
+                      </div>
+                      :
+                      <div className='d-flex'>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info me-3 play-button' onClick={() => onResult()}>
+                            <img src='/assets/play.png' width={100} alt='Play'/>
+                          </div>
+                        </div>
+                        <div className='col-sm-6'>
+                          <div className='alert alert-info ms-3 play-button' onClick={() => onClaim()}>
+                            <img src='/assets/cancel.png' width={100} alt='Cancel'/>
+                          </div>
+                        </div>
+                      </div>
+
+                    }
+                </div>
+                
+              </div>
+
+            </div>
+
+          </div>
+        </main>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+import React from 'react';
 import Header from './Header';
 
 function App() {
@@ -13,3 +248,5 @@ function App() {
 }
 
 export default App;
+
+*/
--- src/Web3Service.ts
+++ src/Web3Service.ts
@@ -9,6 +9,20 @@ type LoginResult = {
     isAdmin: boolean;
 }
 
+export type GameData = {
+    hashOptionP1: string; // Hash of Player 1's option
+    timeOut: string;      // Timeout duration in seconds (uint64)
+    timeOutP1: string;    // Player 1 timeout timestamp (uint256)
+    timeOutP2: string;    // Player 2 timeout timestamp (uint256)
+    nLockTime: string;    // Lock time for the game (uint256)
+    isOdd: boolean;       // Whether the game is Odd/Even (bool)
+    player1: string;      // Player 1's address (address)
+    player2: string;      // Player 2's address (address)
+    optionP2: number;     // Player 2's option (int8)
+    optionP1: number;     // Player 1's option (int8)
+    keyGame: string;      // Player 1's keygame
+  };
+
 const CONTRACT_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`;
 
 function getWeb3(): Web3 {
@@ -107,3 +121,175 @@ export async function setBid(newBid: string): Promise<string>{
     const tx = await contract.methods.setBid(newBid).send();
     return tx.transactionHash;
 }
+
+export enum Options {
+    EVEN = 0,
+    ODD = 1,
+} // 0, 1, 2, 3 - definir os números de forma explicita é opcional
+
+export let gameData: GameData = {
+    hashOptionP1: "0x0000000000000000000000000000000000000000000000000000000000000000",
+    timeOut: "",
+    timeOutP1: "",
+    timeOutP2: "",
+    nLockTime: "",
+    isOdd: false,
+    player1: "",
+    player2: "",
+    optionP2: -1,
+    optionP1: -1,
+    keyGame: ""
+  };
+
+export async function gameState() : Promise <GameData> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    //gameData = fetchGameData(await contract.methods.gameData().call());
+
+    return fetchGameData(await contract.methods.gameData().call());
+}
+
+export async function insertGameKey(gameKeyIn: string) : Promise <string> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    console.log("gameData.optionP1:", gameData.optionP1);
+    if(gameData.optionP1 != -1)
+        throw new Error("Player 1 already started game.");
+
+    //let keySeed = hexStringToUint8Array (gameKeyIn)
+    //let gameKey = web3.utils.keccak256(keySeed);
+
+    gameData.keyGame = gameKeyIn;
+
+    console.log("Game Key:", gameData.keyGame);
+    return gameData.keyGame;
+}
+
+export async function insertPlayerOption(playerOption: number, player1: boolean) : Promise <Number> {
+
+    console.log("gameData.optionP1:", gameData.optionP1);
+
+    if(player1 && gameData.keyGame == "")
+        throw new Error("Player 1 must insert his keygame first.");
+
+    //let keySeed = hexStringToUint8Array (gameKeyIn)
+    //let gameKey = web3.utils.keccak256(keySeed);
+    player1 ? gameData.optionP1 = playerOption : gameData.optionP2 = playerOption;
+
+    return player1 ? gameData.optionP1: gameData.optionP2;
+}
+
+export async function initGame(isOdd: boolean) : Promise <GameData> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    if(gameData.keyGame == "" || gameData.optionP1 == -1)
+        throw new Error("Player 1 must insert his keygame and option first.");
+
+    let keygame = gameData.keyGame;
+    let optionP1In = gameData.optionP1;
+    let optionP1str = optionP1In.toString(16);
+
+    while(optionP1str.length % 2 === 1 )
+      optionP1str = "0" + optionP1str;
+
+    let hashOptionP1In = (web3.utils.keccak256(hexStringToUint8Array(keygame + optionP1str)));
+
+    let bidMin = await contract.methods.bidMin().call(); 
+
+    await contract.methods.playerInit(isOdd, hashOptionP1In).send({value: String(bidMin)});
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    return gameData;
+}
+
+export async function cancel() : Promise <GameData> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    if(gameData.player1 == "0x0000000000000000000000000000000000000000" || gameData.optionP2 != -1)
+        throw new Error("Cannot cancel this game.");
+
+    await contract.methods.quitGame().send();
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    return gameData;
+}
+
+export async function acceptGame() : Promise <GameData> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    console.log("gameData.hashOptionP1: ", gameData.hashOptionP1);
+    console.log("gameData.optionP2: ", gameData.optionP2);
+
+    if(gameData.optionP2 == -1)
+        throw new Error("Player 2 must insert his option first.");
+
+    let bidMin = await contract.methods.bidMin().call(); 
+
+    await contract.methods.acceptGame(gameData.optionP2).send({value: String(bidMin)});
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    return gameData;
+}
+
+export async function resultGame() : Promise <GameData> {
+    const web3 = getWeb3();
+    const contract = getContract(web3);
+
+    if(gameData.keyGame == "" || gameData.optionP1 == -1)
+        throw new Error("Player 1 must insert his keygame and option first.");
+
+    let keygame = gameData.keyGame;
+    let optionP1In = gameData.optionP1;
+    
+    await contract.methods.resultGame(hexStringToUint8Array(keygame), optionP1In).send();
+
+    gameData = fetchGameData(await contract.methods.gameData().call());
+
+    return gameData;
+}
+ 
+export function fetchGameData(rawGameData: any) {
+    
+    const gameData: GameData = {
+      hashOptionP1: rawGameData[0],
+      timeOut: rawGameData[1],
+      timeOutP1: rawGameData[2],
+      timeOutP2: rawGameData[3],
+      nLockTime: rawGameData[4],
+      isOdd: rawGameData[5],
+      player1: rawGameData[6],
+      player2: rawGameData[7],
+      optionP2: Number(rawGameData[8]),
+      optionP1: Number(rawGameData[9]),
+      keyGame: rawGameData[10]
+    };
+    return gameData;
+  }
+  
+  export function hexStringToUint8Array(hexString: string): Uint8Array {
+    // Ensure the hex string length is even
+    if (hexString.length % 2 !== 0) {
+        throw new Error("Hex string must have an even length");
+    }
+  
+    // Convert the string into an array of bytes
+    const byteArray = new Uint8Array(hexString.length / 2);
+    for (let i = 0; i < byteArray.length; i++) {
+        const byte = hexString.substr(i * 2, 2);
+        byteArray[i] = parseInt(byte, 16);
+    }
+  
+    return byteArray;
+  }
